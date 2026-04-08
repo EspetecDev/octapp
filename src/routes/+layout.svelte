5503bbcbc812aa21ed051d06fdbc7b9389aa5deb
Manel Hidalgo Agraz <espetec@Rivendell.local>
2026-04-08 16:36:14 +0200

    fix(01-04): re-mount LandscapeOverlay to satisfy MOBX-04

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "../app.css";
   import { onMount, onDestroy } from "svelte";
   import ReconnectingOverlay from "$lib/ReconnectingOverlay.svelte";
+  import LandscapeOverlay from "$lib/LandscapeOverlay.svelte";
   import { createSocket, destroySocket } from "$lib/socket.ts";
 
   onMount(() => {
@@ -15,6 +16,7 @@
 
 <!-- Overlays sit above all page content at all times -->
 <ReconnectingOverlay />
+<LandscapeOverlay />
 
 <!-- Page content -->
 <slot />
