208b2d2c60d03579e3583e88c4bdd63dfdd46806
Manel Hidalgo Agraz <espetec@Rivendell.local>
2026-04-08 16:27:36 +0200

    fix(01-04): fix viewport width and remove landscape block
    - Add width: 100% and min-height: 100% to html/body so views fill viewport
    - Remove LandscapeOverlay import and usage from layout (landscape supported)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import "../app.css";
   import { onMount, onDestroy } from "svelte";
   import ReconnectingOverlay from "$lib/ReconnectingOverlay.svelte";
-  import LandscapeOverlay from "$lib/LandscapeOverlay.svelte";
   import { createSocket, destroySocket } from "$lib/socket.ts";
 
   onMount(() => {
@@ -16,7 +15,6 @@
 
 <!-- Overlays sit above all page content at all times -->
 <ReconnectingOverlay />
-<LandscapeOverlay />
 
 <!-- Page content -->
 <slot />
